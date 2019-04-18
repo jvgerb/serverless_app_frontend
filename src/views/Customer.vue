3b06e88126e7a69b9c4394ed58b743c7a2c4d034
Nizami <nizami.dev@gmail.com>
2019-04-18 08:15:57 +0300

    ES-15 Added  scaffolding files

---
+++ src/views/Customer.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class Customer extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/vars";
+</style>
