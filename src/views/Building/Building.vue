2103d5baf58c9cbf31242343e97b2abb74f75910
Nizami <nizami.dev@gmail.com>
2019-04-25 14:40:07 +0300

    ES-19 Fisnished Building page

--- src/views/Building/Building.vue
+++ src/views/Building/Building.vue
@@ -2,64 +2,78 @@
   <FormCard>
     <div class="form-row">
       <div class="form-field">
-        <label>Supplier ID</label>
-        <input type="text">
+        <label>Building ID</label>
+        <input type="text" placeholder="342434">
       </div>
 
       <div class="form-field">
-        <label>Type</label>
-        <select>
-          <option value="Heat">- Select Option -</option>
-        </select>
-      </div>
-
-      <div class="form-field">
-        <label>Industry Sector</label>
-        <select>
-          <option value="Heat">- Select Option -</option>
-        </select>
-      </div>
-    </div>
-
-    <div class="form-row">
-      <div class="form-field">
-        <label>Name</label>
-        <input type="text">
-      </div>
-
-      <div class="form-field">
-        <label>Street Address</label>
-        <input type="text">
-      </div>
-
-      <div class="form-field">
-        <label>Street Number</label>
-        <input type="text">
-      </div>
-    </div>
-
-    <div class="form-row">
-      <div class="form-field">
-        <label>Zip Code</label>
-        <input type="text">
-      </div>
-
-      <div class="form-field">
-        <label>Place</label>
-        <input type="text">
+        <label>Building Name</label>
+        <input type="text" placeholder="Steinberg">
       </div>
       <hr>
     </div>
 
     <div class="form-row">
       <div class="form-field">
-        <label>Quality of Relationship</label>
-        <Rating></Rating>
+        <label>Street</label>
+        <input type="text" placeholder="Oraniendamm">
       </div>
 
       <div class="form-field">
-        <label>Homepage</label>
-        <input type="text" placeholder="http://">
+        <label>Number of Entrances</label>
+        <input type="text" placeholder="2">
+      </div>
+
+      <div class="form-field">
+        <label>Category</label>
+        <select>
+          <option value="Heat">Apartment Building</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-field">
+        <label>Street Number</label>
+        <select>
+          <option value="Heat">6 - 10</option>
+        </select>
+      </div>
+
+      <div class="form-field">
+        <label>Number of Floors</label>
+        <input type="text" placeholder="5">
+      </div>
+
+      <div class="form-field">
+        <label>heating_area_total (m^2)</label>
+        <input type="text" placeholder="1250">
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-field">
+        <label>Zip Code</label>
+        <input type="text" placeholder="13469">
+      </div>
+
+      <div class="form-field">
+        <label>Number of Apartments</label>
+        <input type="text" placeholder="32">
+      </div>
+
+      <div class="form-field">
+        <label>Year</label>
+        <select>
+          <option value="Heat">1980</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-field">
+        <label>Place</label>
+        <input type="text" placeholder="Berlin">
       </div>
       <hr>
     </div>
